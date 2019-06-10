d31612195916be7baafc23855b887527b6a3245a
Aitor Lozano <aike89@gmail.com>
2019-06-10 08:36:03 +0100

    Singleton scriptable

---
+++ Scripts/Tools/SingletonScriptableObject.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Abstract class for making reload-proof singletons out of ScriptableObjects
+/// Returns the asset created on editor, null if there is none
+/// Based on https://www.youtube.com/watch?v=VBA1QCoEAX4
+/// </summary>
+/// <typeparam name="T">Type of the singleton</typeparam>
+
+public abstract class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
+{
+	static T _instance = null;
+	public static T instance
+	{
+		get
+		{
+			if (!_instance)
+				_instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+			if (!_instance)
+				_instance = Resources.Load<T>("SingletonScriptables/");
+			return _instance;
+		}
+	}
+}
