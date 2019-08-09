5916906816a6ddd74b1bba88e0a70aa8bda7a0d5
Aitor Lozano <aike89@gmail.com>
2019-08-09 17:46:57 +0100

    Choose popup included

--- Scripts/Menu/OverlayPopup.cs
+++ Scripts/Menu/OverlayPopup.cs
@@ -31,7 +31,8 @@ public class OverlayPopup : MonoBehaviour
 	{
 		closed,
 		loading,
-		showMessage
+		showMessage,
+		showChoose
 	}
 	FSMObject<states> fsm;
 	bool initialized = false;
@@ -50,6 +51,14 @@ public class OverlayPopup : MonoBehaviour
 	public TextMeshProUGUI messageTitle;
 	public TextMeshProUGUI messageBody;
 
+	[Header("Choose popup")]
+	public RectTransform choosePopup;
+	public TextMeshProUGUI chooseTitle;
+	public TextMeshProUGUI chooseBody;
+	Action yesCallback = null;
+	Action noCallback = null;
+
+	[Header("General settings")]
 	public float screenOffset = 900;
 	public float rotateSpeed = 50;
 
@@ -69,6 +78,8 @@ public class OverlayPopup : MonoBehaviour
 		fsm = new FSMObject<states>(this);
 		loadingPopup.anchoredPosition = Vector3.down * screenOffset;
 		messagePopup.anchoredPosition = Vector3.down * screenOffset;
+		choosePopup.anchoredPosition = Vector3.down * screenOffset;
+
 		background.interactable = false;
 		background.blocksRaycasts = false;
 		background.alpha = 0;
@@ -100,7 +111,16 @@ public class OverlayPopup : MonoBehaviour
 		fsm.ChangeState(states.showMessage);
 	}
 
-	public void CloseMessage()
+	public void ShowChoose(string title, string message, Action yesCallback = null, Action noCallback = null)
+	{
+		this.yesCallback = yesCallback;
+		this.noCallback = noCallback;
+		chooseTitle.text = I18n.T(title);
+		chooseBody.text = I18n.T(message);
+		fsm.ChangeState(states.showChoose);
+	}
+
+	public void ClosePopup()
 	{
 		fsm.ChangeState(states.closed);
 	}
@@ -129,39 +149,12 @@ public class OverlayPopup : MonoBehaviour
 		background.blocksRaycasts = true;
 	}
 
-	IEnumerator loading()
+	IEnumerator showView(RectTransform holder, states state)
 	{
 		yield return this.DoTween01(t =>
 		{
 			//loadingPopup.localScale = Vector3.one * PennerAnimation.QuadEaseInOut(t, 0, 1, 1);
-			loadingPopup.localPosition = new Vector3(
-				0,
-				PennerAnimation.BackEaseOut(t, -screenOffset, screenOffset, 1),
-				0
-			);
-		}, 0.75f);
-
-		while (fsm.currentState == states.loading)
-			yield return 0;
-
-		this.DoTween01(t =>
-		{
-			//loadingPopup.localScale = Vector3.one * PennerAnimation.QuadEaseInOut(t, 1, -1, 1);
-			loadingPopup.localPosition = new Vector3(
-				0,
-				PennerAnimation.BackEaseIn(t, 0, screenOffset, 1),
-				0
-			);
-		}, 0.5f);
-	}
-
-	IEnumerator showMessage()
-	{
-
-		yield return this.DoTween01(t =>
-		{
-			//messagePopup.localScale = Vector3.one * PennerAnimation.QuadEaseInOut(t, 0, 1, 1);
-			messagePopup.anchoredPosition = new Vector3(
+			holder.localPosition = new Vector3(
 				0,
 				PennerAnimation.BackEaseOut(t, -screenOffset, screenOffset, 1),
 				0
@@ -171,20 +164,46 @@ public class OverlayPopup : MonoBehaviour
 		background.interactable = true;
 		background.blocksRaycasts = true;
 
-		while (fsm.currentState == states.showMessage)
+		while (fsm.currentState == state)
 			yield return 0;
 
 		this.DoTween01(t =>
 		{
-			//messagePopup.localScale = Vector3.one * PennerAnimation.QuadEaseInOut(t, 1, -1, 1);
-			messagePopup.anchoredPosition = new Vector3(
+			//loadingPopup.localScale = Vector3.one * PennerAnimation.QuadEaseInOut(t, 1, -1, 1);
+			holder.localPosition = new Vector3(
 				0,
 				PennerAnimation.BackEaseIn(t, 0, screenOffset, 1),
 				0
 			);
 		}, 0.5f);
+	}
 
+	IEnumerator loading()
+	{
+		yield return StartCoroutine(showView(loadingPopup, states.loading));
+	}
+
+	IEnumerator showMessage()
+	{
+		yield return StartCoroutine(showView(messagePopup, states.showMessage));
 		callback?.Invoke();
+	}
+
+	IEnumerator showChoose()
+	{
+		yield return StartCoroutine(showView(choosePopup, states.showChoose));
+	}
+
+	public void YesButton()
+	{
+		yesCallback?.Invoke();
+		ClosePopup();
+	}
+
+	public void NoButton()
+	{
+		noCallback?.Invoke();
+		ClosePopup();
 	}
 
 }
