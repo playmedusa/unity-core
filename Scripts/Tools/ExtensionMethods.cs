4490724153d632e6c3986bd526aaf75055c66213
Aitor Lozano <aike89@gmail.com>
2019-09-11 15:21:41 +0100

    Remap extensions

--- Scripts/Tools/ExtensionMethods.cs
+++ Scripts/Tools/ExtensionMethods.cs
@@ -14,6 +14,16 @@ public static class ExtensionMethods
 		return Mathf.RoundToInt(Random.Range(minmax.x, minmax.y));
 	}
 
+	public static float Remap(this float value, float from1, float to1, float from2, float to2)
+	{
+		return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+	}
+
+	public static float RemapClamped(this float value, float from1, float to1, float from2, float to2)
+	{
+		return Mathf.Clamp((value - from1) / (to1 - from1) * (to2 - from2) + from2, from2, to2);
+	}
+
 	public static Color SetAlpha(this Color c, float alpha)
 	{
 		c.a = alpha;
