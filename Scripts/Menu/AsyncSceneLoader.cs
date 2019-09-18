20d3ec0020ba02d1a8ee7fbca8d6c9ef69011902
Aitor Lozano <aike89@gmail.com>
2019-09-18 12:00:31 +0100

    ASync scene loader and FSM object cb support

---
+++ Scripts/Menu/AsyncSceneLoader.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class AsyncSceneLoader : Singleton<AsyncSceneLoader>
+{
+
+	override protected void Init()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	IEnumerator LoadScene(string sceneName, UnityAction callback = null)
+	{
+		var asyncScene = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+		float _loadingProgress;
+
+		// this value stops the scene from displaying when it's finished loading
+		asyncScene.allowSceneActivation = false;
+
+		while (!asyncScene.isDone)
+		{
+			// loading bar progress
+			_loadingProgress = Mathf.Clamp01(asyncScene.progress / 0.9f) * 100;
+
+			// scene has loaded as much as possible, the last 10% can't be multi-threaded
+			if (asyncScene.progress >= 0.9f)
+			{
+				// we finally show the scene
+				asyncScene.allowSceneActivation = true;
+			}
+
+			yield return null;
+		}
+		callback?.Invoke();
+	}
+
+	IEnumerator UnloadScene(string sceneName, UnityAction callback = null)
+	{
+		var asyncScene = SceneManager.UnloadSceneAsync(sceneName);
+		float _unloadingProgress;
+
+		// this value stops the scene from displaying when it's finished loading
+		asyncScene.allowSceneActivation = false;
+
+		while (!asyncScene.isDone)
+		{
+			// loading bar progress
+			_unloadingProgress = Mathf.Clamp01(asyncScene.progress / 0.9f) * 100;
+			// scene has loaded as much as possible, the last 10% can't be multi-threaded
+			if (asyncScene.progress >= 0.9f)
+			{
+				// we finally show the scene
+				asyncScene.allowSceneActivation = true;
+			}
+
+			yield return null;
+		}
+		callback?.Invoke();
+	}
+
+	public void DoLoadScene(string sceneName, UnityAction callback = null)
+	{
+		StartCoroutine(LoadScene(sceneName, callback));
+	}
+
+	public void DoUnloadScene(string sceneName, UnityAction callback = null)
+	{
+		StartCoroutine(UnloadScene(sceneName, callback));
+	}
+
+	public void ShowVeil(UnityAction callback = null)
+	{
+		StartCoroutine(LoadScene("LoadingScreen", () =>
+		{
+			TransitionVeil.instance.ShowVeil(callback);
+		}));
+	}
+
+	public void HideVeil(UnityAction callback = null)
+	{
+		TransitionVeil.instance.HideVeil(() =>
+		{
+			StartCoroutine(UnloadScene("LoadingScreen", callback));
+		});
+	}
+
+}
--- Scripts/Tools/FSMObject.cs
+++ Scripts/Tools/FSMObject.cs
@@ -9,7 +9,6 @@ using System;
 
 public class FSMObject<T>
 {
-
 	public FSMObject(MonoBehaviour owner)
 	{
 		this.owner = owner;
@@ -20,27 +19,34 @@ public class FSMObject<T>
 		get;
 		protected set;
 	}
-	protected UnityAction callback = null;
+	protected Dictionary<T, UnityAction> callback = new Dictionary<T, UnityAction>();
+
 	protected Coroutine fsm = null;
 	MonoBehaviour owner;
 
 	protected IEnumerator FSMLoop()
 	{
+		T lastState;
 		while (Application.isPlaying)
 		{
+			lastState = currentState;
 			yield return owner.StartCoroutine(currentState.ToString());
-			if (callback != null)
-				callback.Invoke();
-			callback = null;
+			if (callback[lastState] != null)
+			{
+				callback[lastState].Invoke();
+				callback[lastState] = null;
+			}
 		}
 	}
 
-	public virtual void ChangeState(T nextState)
+	public virtual void ChangeState(T nextState, UnityAction onChangeStateCallback = null)
 	{
+		this.callback[nextState] = onChangeStateCallback;
 		currentState = nextState;
 		if (fsm == null)
 		{
 			fsm = owner.StartCoroutine(FSMLoop());
 		}
 	}
+
 }
