430798cc1642ecf1e77c4d984b2b1bcd9ca74cda
Aitor Lozano <info@aitorlozano.com>
2021-02-02 11:24:31 +0000

    Included new input system mapper

---
+++ Scripts/Input/ActionMapper.cs
@@ -0,0 +1,92 @@
+
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+
+public class ActionMapper: MonoBehaviour
+{
+    public InputUser user { get; set; }
+    public IInputActionCollection actionCollection { get; private set; }
+
+    private Type currentSchema;
+
+    private void Awake()
+    {
+        InputUser.onChange += InputUserOnChange;
+    }
+
+    private void OnDestroy()
+    {
+        InputUser.onChange -= InputUserOnChange;
+        if (user.valid)
+            DisableCurrentSchema(actionCollection);
+    }
+    
+    private void InputUserOnChange(InputUser inputUser, InputUserChange inputUserChange, InputDevice inputDevice)
+    {
+        if (inputUser != user)
+            return;
+        
+        switch (inputUserChange)
+        {
+            case InputUserChange.Removed:
+                Destroy(gameObject);
+                break;
+            case InputUserChange.DeviceUnpaired:
+                DisableCurrentSchema(actionCollection);
+                break;
+            case InputUserChange.DevicePaired:
+                EnableCurrentSchema(actionCollection);
+                break;
+        }
+    }
+    
+    public void SetCallbacks<T>(Component handler, IInputActionCollection actionCollection)
+    {
+        this.actionCollection = actionCollection;
+        
+        if (currentSchema != null)
+        {
+            var properties= actionCollection.GetType().GetProperties();
+            var wrapperInfo = properties.First(p => p.PropertyType == currentSchema);
+            var oldWrapper = (T) wrapperInfo.GetValue(actionCollection);
+            var disable= oldWrapper.GetType().GetMethod("Disable");
+            disable.Invoke(oldWrapper, null);
+        }
+        currentSchema = typeof(T);
+
+        var wrapper = GetWrapper<T>(actionCollection);
+        var setCallbacks= wrapper.GetType().GetMethod("SetCallbacks");
+        var enable= wrapper.GetType().GetMethod("Enable");
+        setCallbacks.Invoke(wrapper, new object[] {handler});
+        enable.Invoke(wrapper, null);
+    }
+
+    public void DisableCurrentSchema(IInputActionCollection actionCollection)
+    {
+        var properties= actionCollection.GetType().GetProperties();
+        var wrapperInfo = properties.First(p => p.PropertyType == currentSchema);
+        var oldWrapper = wrapperInfo.GetValue(actionCollection);
+        var disable= oldWrapper.GetType().GetMethod("Disable");
+        disable.Invoke(oldWrapper, null);
+    }
+    
+    public void EnableCurrentSchema(IInputActionCollection actionCollection)
+    {
+        var properties= actionCollection.GetType().GetProperties();
+        var wrapperInfo = properties.First(p => p.PropertyType == currentSchema);
+        var oldWrapper = wrapperInfo.GetValue(actionCollection);
+        var enable= oldWrapper.GetType().GetMethod("Enable");
+        enable.Invoke(oldWrapper, null);
+    }
+
+    T GetWrapper<T>(IInputActionCollection actionCollection)
+    {
+        var properties= actionCollection.GetType().GetProperties();
+        var wrapperInfo = properties.First(p => p.PropertyType == typeof(T));
+        var wrapper = (T) wrapperInfo.GetValue(actionCollection);
+        return wrapper;
+    }
+}
---
+++ Scripts/Input/PlayerDeviceSelection.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Users;
+
+public class PlayerDeviceSelection : MonoBehaviour
+{
+    public delegate void NewPlayer(InputUser inputUser);
+    public static event NewPlayer OnNewPlayer;
+    
+    public delegate void DeviceUpdated(InputUser inputUser);
+    public static event DeviceUpdated OnDeviceUpdated;
+        
+    static PlayerDeviceSelection _instance;
+    public static PlayerDeviceSelection instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<PlayerDeviceSelection>();
+            return _instance;
+        }
+    }
+
+    public enum state
+    {
+        listenMainPlayer,
+        bindPlayers,
+        lockDevices
+    }
+
+    private state currentState;
+
+    public void SetState(state nextState)
+    {
+        currentState = nextState;
+    }
+
+    public int maxPlayers
+    {
+        get => _maxPlayers;
+        set
+        {
+            _maxPlayers = value;
+            InputUser.listenForUnpairedDeviceActivity = _maxPlayers;
+        }
+    }
+    public int _maxPlayers = 1;
+    
+    public Func<InputControl, bool> pairDeviceCheck;
+
+    void Awake()
+    {
+        InputUser.listenForUnpairedDeviceActivity = 1;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnEnable()
+    {
+        InputUser.onUnpairedDeviceUsed += InputUserOnUnpairedDeviceUsed;
+    }
+
+    void OnDisable()
+    {
+        InputUser.onUnpairedDeviceUsed -= InputUserOnUnpairedDeviceUsed;
+    }
+    
+    public void RemoveAllUsers()
+    {
+        for (int i = 0; i < InputUser.all.Count; i++)
+        {
+            InputUser.all[i].UnpairDevicesAndRemoveUser();
+        }
+    }
+
+    public void UnpairAllDevices()
+    {
+        for (int i = 0; i < InputUser.all.Count; i++)
+            InputUser.all[i].UnpairDevices();
+    }
+
+    private void InputUserOnUnpairedDeviceUsed(InputControl inputControl, InputEventPtr inputEventPtr)
+    {
+        if (pairDeviceCheck != null && !pairDeviceCheck.Invoke(inputControl))
+            return;
+        
+        switch (currentState)
+        {
+            case state.listenMainPlayer:
+                PairUserWithDevice(0, inputControl.device);
+                break;
+            case state.bindPlayers:
+                for (var index = 0; index < InputUser.all.Count; index++)
+                {
+                    var inputUser = InputUser.all[index];
+                    if (inputUser.pairedDevices.Count == 0)
+                    {
+                        PairUserWithDevice(index, inputControl.device);
+                        return;
+                    }
+                }
+                if (InputUser.all.Count < maxPlayers)
+                    PairUserWithDevice(InputUser.all.Count, inputControl.device);
+                break;
+            case state.lockDevices:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void PairUserWithDevice(int userIndex, InputDevice device)
+    {
+        InputUser user;
+        if (InputUser.all.Count > userIndex)
+        {
+            user = InputUser.all[userIndex];
+            InputUser.PerformPairingWithDevice(device, user, InputUserPairingOptions.UnpairCurrentDevicesFromUser);
+            OnDeviceUpdated?.Invoke(user);
+        }
+        else
+        {
+            user = InputUser.PerformPairingWithDevice(device);
+            OnNewPlayer?.Invoke(user);
+        }
+    }
+    
+    public GameObject CreatePlayerInput(GameObject playerInputPrefab, int playerIndex)
+    {
+        GameObject obj = Instantiate(playerInputPrefab);
+        SetupPlayerInput(obj, playerIndex);
+        return obj;
+    }
+
+    public void SetupPlayerInput(GameObject playerInputObj, int playerIndex)
+    {
+        ActionMapper actionMapper = playerInputObj.GetComponentInChildren<ActionMapper>();
+        
+        var user = InputUser.all[playerIndex];
+        actionMapper.user = user;
+        actionMapper.user.AssociateActionsWithUser(actionMapper.actionCollection);
+    }
+
+
+}
+
